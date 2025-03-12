960b63c173636fb6df259e1feaaa20df7ecb3e76
Lucas Henrique da Costa Leonardo <lucas.leonardo@nemu.com.br>
2025-03-12 13:51:29 -0300

    configs do redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,14 @@
-module.exports = {
-    host: "127.0.0.1",
-    port: 6379
-  };
-  
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379" 
+});
+
+redisClient.on("error", (err) => console.error("Erro no Redis", err));
+
+(async () => {
+    await redisClient.connect();
+    console.log("🔗 Conectado ao Redis!");
+})();
+
+module.exports = redisClient;
