18ab82a07e6badd0d9d2024ce176a4da5403a15d
Lucas Henrique da Costa Leonardo <lucas.leonardo@nemu.com.br>
2025-03-12 15:23:34 -0300

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,6 @@
 const Redis = require("ioredis");
 
-const redisClient = new Redis(process.env.REDIS_URL, {
+const redisClient = new Redis(process.env.REDIS_URL+ '?family=0', {
     tls: { rejectUnauthorized: false },
     maxRetriesPerRequest: null,
 });
